9a3ab83ead1ba8329ed845dee6b7ba770e4a626e
Tyler Swindell <sprawl_0heights@icloud.com>
2023-06-09 11:15:10 -0400

    Update vue.config.js for Mac builds

--- vue.config.js
+++ vue.config.js
@@ -1,42 +1,18 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   pluginOptions: {
     electronBuilder: {
       nodeIntegration: true,
-      // List native deps here if they don't work
-      externals: ['electron'],
-      // If you are using Yarn Workspaces, you may have multiple node_modules folders
-      // List them all here so that VCP Electron Builder can find them
-      nodeModulesPath: ['./node_modules'],
+      externals: ["electron"],
+      nodeModulesPath: ["./node_modules"],
       builderOptions: {
-        // options placed here will be merged with default configuration and passed to electron-builder
         productName: "RGB-PC",
-        win: {
-          icon: "./rgb.ico",
-          target: [
-            {
-              target: "nsis",
-              arch: [
-                "x64",
-                "ia32"
-              ]
-            },
-            {
-              target: "zip",
-              arch: [
-                "x64",
-                "ia32"
-              ]
-            }
-          ]
+        mac: {
+          category: "public.app-category.entertainment",
+          target: "default",
         },
-        nsis: {
-          oneClick: false,
-          allowToChangeInstallationDirectory: true,
-          installerIcon: "./rgb.ico"
-        }
-      }
-    }
-  }
-})
+      },
+    },
+  },
+});
